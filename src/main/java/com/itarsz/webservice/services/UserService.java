239fa57bc3e1f5354c9f0217da21fe63e342ab12
iTarsz <adhemardev@gmail.com>
2023-03-01 20:00:38 -0300

    Exception handling - update

--- src/main/java/com/itarsz/webservice/services/UserService.java
+++ src/main/java/com/itarsz/webservice/services/UserService.java
@@ -13,6 +13,8 @@ import com.itarsz.webservice.repositories.UserRepository;
 import com.itarsz.webservice.services.exceptions.DatabaseException;
 import com.itarsz.webservice.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -48,9 +50,15 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
