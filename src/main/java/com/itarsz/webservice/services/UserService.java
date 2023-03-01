5e838aec37a4e963ad726865bc30bef6bd3623a6
iTarsz <adhemardev@gmail.com>
2023-03-01 19:52:47 -0300

    Exception handling - delete

--- src/main/java/com/itarsz/webservice/services/UserService.java
+++ src/main/java/com/itarsz/webservice/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.itarsz.webservice.entities.User;
 import com.itarsz.webservice.repositories.UserRepository;
+import com.itarsz.webservice.services.exceptions.DatabaseException;
 import com.itarsz.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,15 @@ public class UserService {
 
 	public void delete(Long id) {
 
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+
+			throw new ResourceNotFoundException(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 
 	}
 
